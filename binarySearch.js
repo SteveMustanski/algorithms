d1add6406b8c6afd2a7e8ed4ab76f373930b4128
Steve Mustanski <steve.mustanski@gmail.com>
2019-01-05 21:40:49 -0500

    fixed formatting in binary search

--- binarySearch.js
+++ binarySearch.js
@@ -1,18 +1,18 @@
 
-numbers = [1,2,3,4,5,6,7,8,9,10];
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function binarySearch (list, target) {
+function binarySearch(list, target) {
   let first = 0;
   let last = list.length - 1;
 
   while (first <= last) {
-    let midpoint = Math.floor((first + last)/2);
+    let midpoint = Math.floor((first + last) / 2);
     if (list[midpoint] === target) {
       return midpoint;
     } else if (list[midpoint] < target) {
       first = midpoint + 1;
     } else {
-      last = midpoint -1;
+      last = midpoint - 1;
     }
   }
   return -1;
